7fd1e5bdeb4ef783240a75f7126e301c94722192
ArunN63 <arunn898989@gmail.com>
2025-08-15 11:42:52 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# Hi
+import os
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()  # Load environment variables from .env file
+client = Groq(
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+print(chat_completion.choices[0].message.content)
